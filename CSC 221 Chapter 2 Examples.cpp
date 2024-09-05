1b25875227f34111f9bf19a860c3d3f079fb2be4
Sally Schaffner <schaffners@vpcc.edu>
2024-09-05 10:31:58 -0400

    Add project files.

---
+++ CSC 221 Chapter 2 Examples.cpp
@@ -0,0 +1,12 @@
+// CSC 221 Chapter 2 Examples.cpp - There will be two programs in this project.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Chapter 2 Examples\n";
+}
+
+
